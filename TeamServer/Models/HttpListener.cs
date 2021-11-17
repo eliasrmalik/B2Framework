81b31208c5c6f5c4a9172860d5358e19a2e00f02
Elias Malik <eliasrmalik@gmail.com>
2021-11-17 05:02:53 -0800

    Init

---
+++ TeamServer/Models/HttpListener.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TeamServer.Models
+{
+    public class HttpListener : Listener
+    {
+        public override string Name { get; }
+        public int BindPort { get; }
+
+        private CancellationTokenSource _tokenSource;
+
+        public HttpListener(string name, int bindPort)
+        {
+            Name = name;
+            BindPort = bindPort;
+        }
+
+
+        public override async Task Start()
+        {
+            var hostBuilder = new HostBuilder()
+                .ConfigureWebHostDefaults(host =>
+                {
+                    host.UseUrls($"http://0.0.0.0:{BindPort}");
+                    host.Configure(ConfigureApp);
+                    host.ConfigureServices(ConfigureServices);
+                });
+           
+            var host = hostBuilder.Build();
+
+            _tokenSource = new CancellationTokenSource();
+
+
+            host.RunAsync(_tokenSource.Token);
+        }
+        private void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+        }
+
+        private void ConfigureApp(IApplicationBuilder app)
+        {
+            app.UseRouting();
+            app.UseEndpoints(e =>
+            {
+                // This where you can do potential malleable C2 configuration 
+                e.MapControllerRoute("/", "/", new { controller = "HttpListener", action = "HandleImplant" });
+            });
+        }
+
+        public override void Stop()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
