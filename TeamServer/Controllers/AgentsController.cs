ac55e0ff872146b8360ca324f4cf15b2a3167319
Elias Malik <eliasrmalik@gmail.com>
2021-11-18 08:59:41 -0800

    Create Agent API Controller

---
+++ TeamServer/Controllers/AgentsController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TeamServer.Services;
+
+namespace TeamServer.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+
+
+    public class AgentsController : ControllerBase
+    {
+        private readonly IAgentService _agents;
+
+        [HttpGet]
+        public IActionResult GetAgents()
+        { 
+        
+            var agents = _agents.GetAgents();
+            return Ok(agents);
+        
+        }
+
+        [HttpGet("{name}")]
+        public IActionResult GetAgents(string name)
+        {
+
+            var agent = _agents.GetAgent(name);
+            if (agent is null) return NotFound();
+            return Ok(agent);
+
+        }
+
+
+    }
+}
