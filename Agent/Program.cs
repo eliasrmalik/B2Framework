ca43e1694624b8f36f9e702c18d92f77957b5897
Elias Malik <eliasrmalik@gmail.com>
2021-11-19 04:17:06 -0800

    Create HTTPComModule class

--- Agent/Program.cs
+++ Agent/Program.cs
@@ -5,6 +5,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Security.Principal;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Agent
@@ -13,10 +14,32 @@ namespace Agent
     {
 
         private static AgentMetadata _metadata;
+        private static CommModule _commModule;
+        private static CancellationTokenSource _tokenSource;
 
         static void Main(string[] args)
         {
             GenerateMetadata();
+            _commModule = new HttpCommModule("localhost", 8080);
+            _commModule.Init(_metadata);
+            _commModule.Start();
+
+            while (!_tokenSource.IsCancellationRequested)
+            {
+                if (_commModule.RecvData(out var tasks))
+                { 
+                    // action tasks 
+                
+                }
+            
+            }
+
+        }
+
+        public void Stop()
+        {
+            _tokenSource.Cancel();
+
         }
 
         static void GenerateMetadata()
