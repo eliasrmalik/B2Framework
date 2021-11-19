ee0f0b460f5de0244d92cbd8272cb74eab5fb120
Elias Malik <eliasrmalik@gmail.com>
2021-11-19 05:49:18 -0800

    Fix task deserialisation issue in Agent

--- Agent/Models/AgentTask.cs
+++ Agent/Models/AgentTask.cs
@@ -1,18 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Agent.Models
 {
+
+    [DataContract]
     public class AgentTask
     {
+
+        [DataMember(Name = "id")]
         public string Id { get; set; }
+
+        [DataMember(Name = "command")]
         public string Command { get; set; }
 
+        [DataMember(Name = "arguments")]
         public string[] Arguments { get; set; }
 
+        [DataMember(Name = "file")]
         public byte[] File { get; set; }
 
 
