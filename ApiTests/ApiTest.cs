e7e48a95e65a6a6155e2abf3ca8ff14d241a108e
Elias Malik <eliasrmalik@gmail.com>
2021-11-26 04:24:59 -0800

    implement API unit tests

---
+++ ApiTests/ApiTest.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using TeamServer;
+
+namespace ApiTests
+{
+    public abstract class ApiTest
+    {
+        protected HttpClient Client;
+
+
+        protected ApiTest()
+        {
+            var factory = new WebApplicationFactory<Startup>();
+            Client = factory.CreateClient();
+        }
+    }
+}
